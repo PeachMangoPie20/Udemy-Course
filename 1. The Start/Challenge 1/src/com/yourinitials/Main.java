acd400fc714035923001de608a4218bf5c48d375
PeachMangoPie20 <jsscmanalastas@gmail.com>
2022-01-13 12:01:18 +0100

    Add files via upload

---
+++ 1. The Start/Challenge 1/src/com/yourinitials/Main.java
@@ -0,0 +1,14 @@
+package com.yourinitials;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("JJJJJ  M   M");
+        System.out.println("  J    MM MM");
+        System.out.println("  J    M M M");
+        System.out.println("  J    M   M");
+        System.out.println("  J    M   M");
+        System.out.println("J J    M   M");
+        System.out.println(" JJ    M   M");
+    }
+}
